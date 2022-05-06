0569928c0994274421667fe1a7b2dbc885b1fd23
Cem Aksoylar <caksoylar@gmail.com>
2022-05-06 11:17:28 -0700

    Add WPM scaffolding

--- app/src/display/widgets/ffkb_fox.c
+++ app/src/display/widgets/ffkb_fox.c
@@ -1,9 +1,15 @@
+#include <zmk/event_manager.h>
+#include <zmk/events/wpm_state_changed.h>
+
 #include <zmk/display/widgets/ffkb_fox.h>
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
-// const void* *images;
-// uint8_t images_len;
+enum anim_state {
+    anim_state_none,
+    anim_state_slow,
+    anim_state_fast
+} current_anim_state;
 
 LV_IMG_DECLARE(fox_mid);
 LV_IMG_DECLARE(fox_down);
@@ -22,12 +28,7 @@ void set_img_src(void *var, lv_anim_value_t val) {
     lv_img_set_src(img, images[val]);
 }
 
-int zmk_widget_fox_init(struct zmk_widget_fox *widget, lv_obj_t *parent) {
-    widget->obj = lv_img_create(parent, NULL);
-    lv_img_set_auto_size(widget->obj, true);
-    // lv_img_set_src(widget->obj, &fox_mid);
-    // lv_obj_align(widget->obj, NULL, LV_ALIGN_CENTER, 0, 0);
-
+void update_fox_wpm(struct zmk_widget_fox *widget, int wpm) { 
     lv_anim_init(&widget->anim);
     lv_anim_set_time(&widget->anim, 1000);
     // lv_anim_set_repeat_delay(&widget->anim, 500);
@@ -37,8 +38,32 @@ int zmk_widget_fox_init(struct zmk_widget_fox *widget, lv_obj_t *parent) {
     lv_anim_set_repeat_count(&widget->anim, LV_ANIM_REPEAT_INFINITE);
 
     lv_anim_start(&widget->anim);
+}
+
+int zmk_widget_fox_init(struct zmk_widget_fox *widget, lv_obj_t *parent) {
+    widget->obj = lv_img_create(parent, NULL);
+    lv_img_set_auto_size(widget->obj, true);
+    // lv_img_set_src(widget->obj, &fox_mid);
+    // lv_obj_align(widget->obj, NULL, LV_ALIGN_CENTER, 0, 0);
+
+    update_fox_wpm(widget, 0);
+
+    sys_slist_append(&widgets, &widget->node);
 
     return 0;
 }
 
 lv_obj_t *zmk_widget_fox_obj(struct zmk_widget_fox *widget) { return widget->obj; }
+
+int fox_listener(const zmk_event_t *eh) {
+    struct zmk_widget_fox *widget;
+    struct zmk_wpm_state_changed *ev = as_zmk_wpm_state_changed(eh);
+    /*SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) {
+        // LOG_DBG("Set the WPM %d", ev->state);
+        update_fox_wpm(widget, ev->state);
+    }*/
+    return ZMK_EV_EVENT_BUBBLE;
+}
+
+ZMK_LISTENER(zmk_widget_fox, fox_listener)
+ZMK_SUBSCRIPTION(zmk_widget_fox, zmk_wpm_state_changed);
