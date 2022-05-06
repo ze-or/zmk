3e0aedeb15ec2da641764013815eb6f14363a0d6
Cem Aksoylar <caksoylar@gmail.com>
2022-05-06 10:47:13 -0700

    WIP animated fox widget

--- app/src/display/ffkb_fox_widget.h
+++ app/src/display/ffkb_fox_widget.h
@@ -4,7 +4,7 @@
 struct ffkb_fox_widget {
     sys_snode_t node;
     lv_obj_t *obj;
-    // lv_anim_t anim;
+    lv_anim_t anim;
 };
 
 int ffkb_fox_widget_init(struct ffkb_fox_widget *widget, lv_obj_t *parent);
