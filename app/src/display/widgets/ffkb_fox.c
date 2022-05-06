b00c5fbfd306978539b6d4e8b6f5c7049242ca83
Cem Aksoylar <caksoylar@gmail.com>
2022-05-06 11:43:02 -0700

    Add WPM animation switching

--- app/src/display/widgets/ffkb_fox.c
+++ app/src/display/widgets/ffkb_fox.c
@@ -1,12 +1,16 @@
 #include <zmk/event_manager.h>
 #include <zmk/events/wpm_state_changed.h>
 
+#include <logging/log.h>
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
 #include <zmk/display/widgets/ffkb_fox.h>
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
 enum anim_state {
     anim_state_none,
+    anim_state_idle,
     anim_state_slow,
     anim_state_fast
 } current_anim_state;
@@ -29,22 +33,46 @@ void set_img_src(void *var, lv_anim_value_t val) {
 }
 
 void update_fox_wpm(struct zmk_widget_fox *widget, int wpm) { 
-    lv_anim_init(&widget->anim);
-    lv_anim_set_time(&widget->anim, 1000);
-    // lv_anim_set_repeat_delay(&widget->anim, 500);
-    lv_anim_set_var(&widget->anim, widget->obj);
-    lv_anim_set_values(&widget->anim, 0, 3);
-    lv_anim_set_exec_cb(&widget->anim, set_img_src);
-    lv_anim_set_repeat_count(&widget->anim, LV_ANIM_REPEAT_INFINITE);
+    LOG_DBG("anim state %d", current_anim_state);
+    if (wpm == 0) {
+        if (current_anim_state != anim_state_idle) {
+            LOG_DBG("anim state idle");
+            lv_anim_del(widget->obj, set_img_src);
+            lv_img_set_src(widget->obj, &fox_mid);
+            current_anim_state = anim_state_idle;
+        }
+    } else if (wpm < CONFIG_ZMK_WIDGET_FOX_SLOW_LIMIT) {
+        if (current_anim_state != anim_state_slow) {
+            LOG_DBG("anim state slow");
+            lv_anim_init(&widget->anim);
+            lv_anim_set_time(&widget->anim, 1000);
+            lv_anim_set_repeat_delay(&widget->anim, 250);
+            lv_anim_set_var(&widget->anim, widget->obj);
+            lv_anim_set_values(&widget->anim, 0, 3);
+            lv_anim_set_exec_cb(&widget->anim, set_img_src);
+            lv_anim_set_repeat_count(&widget->anim, LV_ANIM_REPEAT_INFINITE);
 
-    lv_anim_start(&widget->anim);
+            lv_anim_start(&widget->anim);
+            current_anim_state = anim_state_slow;
+        }
+    } else if (current_anim_state != anim_state_fast) {
+        LOG_DBG("anim state fast");
+        lv_anim_init(&widget->anim);
+        lv_anim_set_time(&widget->anim, 500);
+        lv_anim_set_repeat_delay(&widget->anim, 125);
+        lv_anim_set_var(&widget->anim, widget->obj);
+        lv_anim_set_values(&widget->anim, 0, 3);
+        lv_anim_set_exec_cb(&widget->anim, set_img_src);
+        lv_anim_set_repeat_count(&widget->anim, LV_ANIM_REPEAT_INFINITE);
+
+        lv_anim_start(&widget->anim);
+        current_anim_state = anim_state_fast;
+    }
 }
 
 int zmk_widget_fox_init(struct zmk_widget_fox *widget, lv_obj_t *parent) {
     widget->obj = lv_img_create(parent, NULL);
     lv_img_set_auto_size(widget->obj, true);
-    // lv_img_set_src(widget->obj, &fox_mid);
-    // lv_obj_align(widget->obj, NULL, LV_ALIGN_CENTER, 0, 0);
 
     update_fox_wpm(widget, 0);
 
@@ -58,10 +86,10 @@ lv_obj_t *zmk_widget_fox_obj(struct zmk_widget_fox *widget) { return widget->obj
 int fox_listener(const zmk_event_t *eh) {
     struct zmk_widget_fox *widget;
     struct zmk_wpm_state_changed *ev = as_zmk_wpm_state_changed(eh);
-    /*SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) {
-        // LOG_DBG("Set the WPM %d", ev->state);
+    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) {
+        LOG_DBG("Set the WPM %d", ev->state);
         update_fox_wpm(widget, ev->state);
-    }*/
+    }
     return ZMK_EV_EVENT_BUBBLE;
 }
 
