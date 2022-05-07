89dd183fe8b0e7652fd63ae704dce702a2fde14d
Cem Aksoylar <caksoylar@gmail.com>
2022-05-07 12:57:23 -0700

    Replace wifi icon with custom BT

--- app/include/zmk/display/widgets/output_status.h
+++ app/include/zmk/display/widgets/output_status.h
@@ -12,6 +12,7 @@
 struct zmk_widget_output_status {
     sys_snode_t node;
     lv_obj_t *obj;
+    lv_obj_t *img;
 };
 
 int zmk_widget_output_status_init(struct zmk_widget_output_status *widget, lv_obj_t *parent);
