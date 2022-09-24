e61b4b058977ad7c5a2aac65089d30c35c491e29
Cem Aksoylar <caksoylar@gmail.com>
2022-09-23 22:36:34 -0700

    Use v2 status screen from lowprokb/zmk#2

--- app/boards/arm/corne-ish_zen/widgets/layer_status.h
+++ app/boards/arm/corne-ish_zen/widgets/layer_status.h
@@ -1,9 +1,9 @@
 /*
-*
-* Copyright (c) 2021 Darryl deHaan
-* SPDX-License-Identifier: MIT
-*
-*/
+ *
+ * Copyright (c) 2021 Darryl deHaan
+ * SPDX-License-Identifier: MIT
+ *
+ */
 
 #pragma once
 
@@ -13,7 +13,6 @@
 struct zmk_widget_layer_status {
     sys_snode_t node;
     lv_obj_t *obj;
-    //lv_obj_t *obj2;
 };
 
 int zmk_widget_layer_status_init(struct zmk_widget_layer_status *widget, lv_obj_t *parent);
