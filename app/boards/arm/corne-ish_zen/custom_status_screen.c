68cb45c6721f6bd3aaaf18905c01995722129ee4
Cem Aksoylar <caksoylar@gmail.com>
2022-09-23 22:36:34 -0700

    Use v2 status screen from lowprokb/zmk#2

--- app/boards/arm/corne-ish_zen/custom_status_screen.c
+++ app/boards/arm/corne-ish_zen/custom_status_screen.c
@@ -6,6 +6,7 @@
  */
 
 #include "widgets/battery_status.h"
+#include "widgets/peripheral_status.h"
 #include "widgets/output_status.h"
 #include "widgets/layer_status.h"
 #include "custom_status_screen.h"
@@ -22,6 +23,10 @@ static struct zmk_widget_battery_status battery_status_widget;
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_OUTPUT_STATUS)
 static struct zmk_widget_output_status output_status_widget;
+#endif
+
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_PERIPHERAL_STATUS)
+static struct zmk_widget_peripheral_status peripheral_status_widget;
 #endif
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS)
@@ -45,26 +50,31 @@ lv_obj_t *zmk_display_status_screen() {
                  41);
 #endif
 
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_PERIPHERAL_STATUS)
+    zmk_widget_peripheral_status_init(&peripheral_status_widget, screen);
+    lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_status_widget), NULL,
+                 LV_ALIGN_IN_TOP_MID, 0, 41);
+#endif
+
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS)
-    // lv_style_set_pad_inner(&layerstyle, LV_STATE_DEFAULT, 12);
-    // lv_obj_add_style(&layer_status_widget, LV_WIDGET_PART_MAIN, &layerstyle);
     zmk_widget_layer_status_init(&layer_status_widget, screen);
+    lv_obj_set_style_local_text_font(zmk_widget_layer_status_obj(&layer_status_widget),
+                                     LV_LABEL_PART_MAIN, LV_STATE_DEFAULT,
+                                     lv_theme_get_font_small());
     lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), NULL, LV_ALIGN_IN_BOTTOM_MID, 0,
                  -5);
-#endif
 
-#if CONFIG_BOARD_CORNEISH_ZEN_RIGHT
-    lv_obj_t *zenlogo_icon;
-    zenlogo_icon = lv_img_create(screen, NULL);
-    lv_img_set_src(zenlogo_icon, &zenlogo);
-    lv_obj_align(zenlogo_icon, NULL, LV_ALIGN_IN_BOTTOM_MID, 2, -5);
-#endif
-
-#if CONFIG_BOARD_CORNEISH_ZEN_LEFT
     lv_obj_t *LayersHeading;
     LayersHeading = lv_img_create(screen, NULL);
     lv_obj_align(LayersHeading, NULL, LV_ALIGN_IN_BOTTOM_MID, 8, 5);
     lv_img_set_src(LayersHeading, &layers2);
+#endif
+
+#if !IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
+    lv_obj_t *zenlogo_icon;
+    zenlogo_icon = lv_img_create(screen, NULL);
+    lv_img_set_src(zenlogo_icon, &zenlogo);
+    lv_obj_align(zenlogo_icon, NULL, LV_ALIGN_IN_BOTTOM_MID, 2, -5);
 #endif
 
     // lv_task_handler();
