17c38007b0a28e47eaa3ff56392f40ce757f4431
Manna Harbour <51143715+manna-harbour@users.noreply.github.com>
2022-10-15 17:47:13 +1100

    Adjust custom status screen layout
    - Center in each third to support different sized widgets without adjustment
    - Add Kconfig to remove static layer label

--- app/boards/arm/corne-ish_zen/custom_status_screen.c
+++ app/boards/arm/corne-ish_zen/custom_status_screen.c
@@ -40,20 +40,34 @@ lv_obj_t *zmk_display_status_screen() {
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_BATTERY_STATUS)
     zmk_widget_battery_status_init(&battery_status_widget, screen);
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS_HIDE_HEADING)
+    lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), NULL, LV_ALIGN_CENTER, 0,
+                 -43);
+#else
     lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), NULL, LV_ALIGN_IN_TOP_MID,
                  0, 2);
+#endif
 #endif
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_OUTPUT_STATUS)
     zmk_widget_output_status_init(&output_status_widget, screen);
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS_HIDE_HEADING)
+    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), NULL, LV_ALIGN_CENTER, 0, 0);
+#else
     lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), NULL, LV_ALIGN_IN_TOP_MID, 0,
                  41);
+#endif
 #endif
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_PERIPHERAL_STATUS)
     zmk_widget_peripheral_status_init(&peripheral_status_widget, screen);
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS_HIDE_HEADING)
+    lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_status_widget), NULL, LV_ALIGN_CENTER,
+                 0, 0);
+#else
     lv_obj_align(zmk_widget_peripheral_status_obj(&peripheral_status_widget), NULL,
                  LV_ALIGN_IN_TOP_MID, 0, 41);
+#endif
 #endif
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS)
@@ -61,6 +75,9 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_set_style_local_text_font(zmk_widget_layer_status_obj(&layer_status_widget),
                                      LV_LABEL_PART_MAIN, LV_STATE_DEFAULT,
                                      lv_theme_get_font_small());
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS_HIDE_HEADING)
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), NULL, LV_ALIGN_CENTER, 0, 43);
+#else
     lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), NULL, LV_ALIGN_IN_BOTTOM_MID, 0,
                  -5);
 
@@ -69,12 +86,17 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_align(LayersHeading, NULL, LV_ALIGN_IN_BOTTOM_MID, 8, 5);
     lv_img_set_src(LayersHeading, &layers2);
 #endif
+#endif
 
 #if !IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
     lv_obj_t *zenlogo_icon;
     zenlogo_icon = lv_img_create(screen, NULL);
     lv_img_set_src(zenlogo_icon, &zenlogo);
+#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS_HIDE_HEADING)
+    lv_obj_align(zenlogo_icon, NULL, LV_ALIGN_CENTER, 0, 43);
+#else
     lv_obj_align(zenlogo_icon, NULL, LV_ALIGN_IN_BOTTOM_MID, 2, -5);
+#endif
 #endif
 
     // lv_task_handler();
